c6bc7823e3411b5559ed2e326181a97c593c76c3
Aleksandr K <truehot@hot.ee>
2015-06-22 09:05:17 +0300

    example pages updated

--- tests/small.filter.request.js
+++
@@ -1,9 +0,0 @@
-QUnit.test("Filter.Request", function (assert) {
-    //var columnModel = new Small.Column.Create({}, {});
-    //var dataModel = new Small.Data.Create({}, {});
-    //var settingsModel = new Small.Settings.Create({});
-    //var view = new Small.View.Create({});
-    assert.ok(true, "No tests");
-});
-
-
--- tests/small.view.model.js
+++
@@ -1,6 +0,0 @@
-QUnit.test("View.Model", function (assert) {
-
-    assert.ok(true, "No tests");
-});
-
-
