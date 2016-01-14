80a38b4744fddaa92a88834c179260739caf771e
Aleksandr K <truehot@hot.ee>
2016-01-14 21:14:35 +0200

    readme update

--- readme.md
+++ readme.md
@@ -1,16 +1,61 @@
-SmallGrid is simple javascript component for displaying tabular data.
-=========
-Alfa version.
+# SmallGrid
+SmallGrid (beta) is simple javascript component for displaying tabular data.
 
+## Demo
 
-Jquery 2.3.1
+> [Demo link](http://truehot.github.io/smallGrid/).
 
+## Requires
 
-Chrome latest<br>
-Firefox latest<br>
-IE latest beta suport<br>
+- jQuery >= 2.1.4
+- Font awesome >=4.3.0
 
+## Browser Support
 
-Qunit used for tests
+IE 12+ ✔ | Chrome 47.0+ ✔ | Firefox 43.0+ ✔|
 
-Examples: <a href="http://truehot.github.io/smallGrid/">truehot.github.io/smallGrid/</a>
+## Installation
+
+Include necessary JS and Css files
+
+<!-- -->
+
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="styles.all.css" type="text/css" />
+    <script src="libs/jquery-2.1.4.min.js"></script>
+    <script src="small.grid.all.min.js"></script>
+
+## Usage
+
+Example code:
+
+        $(document).ready(function () {
+            var columns = [
+		{ 'name': "Name", 'field': "task1", 'width': 100 },
+            ];
+            var items = [
+		{ "task1": "Eric", "task2": "Ball", "task3": "Tallinn", "task4": "1232", "task5": "1111111" },
+            ];
+            var grid = new SmallGrid.Grid.Create($('.small-grid'), items, columns, { "resizeColumnsOnLoad": true });
+        });
+
+## Tests
+
+Qunit used.
+From the project directory, in folder tests can be ran using index.html
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Contact
+
+For questions about smallGrid, you can write me smallgridinfo@gmail.com or open an issue
+
+## License
+
+[MIT License](https://github.com/truehot/smallGrid/blob/master/LICENSE.txt)
--- tests/core/view.model.js
+++ tests/core/view.model.js
@@ -1,7 +1,60 @@
 QUnit.module("SmallGrid.View");
 QUnit.test("Model", function (assert) {
+    var settings = SmallGrid.Settings.Create();
+    var rowModel = new SmallGrid.Row.Create([], settings);
+    var columnModel = new SmallGrid.Column.Create([], settings);
 
-    assert.ok(true, "No tests");
+    var sorter = new SmallGrid.Query.SorterQuery("test1");
+    var filter = new SmallGrid.Query.FilterQuery('test', settings).and();
+
+    var viewModel = SmallGrid.View.Model.Create(rowModel, columnModel, settings);
+
+    assert.deepEqual(viewModel.getRowsTotal(), { "count": 0, "height": 0 }, "getRowsTotal");
+    assert.deepEqual(viewModel.getColumnsTotal(), { "count": 0, "width": 0 }, "getColumnsTotal");
+
+    assert.deepEqual(viewModel.columns, columnModel, "columnModel");
+    assert.deepEqual(viewModel.rows, rowModel, "rowModel");
+
+    assert.deepEqual(viewModel.requestDataFromRange({ top: 0, left: 0 }, { width: 0, height: 0 }, 0, 0, 0), { "columns": [], "isCached": 0, "rows": [] }, "rowModel");
+
+    //sorters
+    viewModel.setSorter(sorter);
+    assert.ok(viewModel.getSorters()[0].getField() == sorter.getField(), "setSorter && getSorters");
+
+    viewModel.clearSorter(sorter);
+    assert.deepEqual(viewModel.getSorters(), [], "clearSorter");
+
+    viewModel.setSorter(sorter);
+    viewModel.clearSorters();
+    assert.deepEqual(viewModel.getSorters(), [], "clearSorters");
+
+
+    //filters
+    viewModel.setFilter(filter);
+    assert.ok(viewModel.getFilters()[0].get() == filter.get(), "setFilter && getFilters");
+
+    viewModel.clearFilter(filter);
+    assert.deepEqual(viewModel.getFilters(), [], "clearFilter");
+
+    viewModel.setFilter(filter);
+    viewModel.clearFilters();
+    assert.deepEqual(viewModel.getFilters(), [], "clearFilters");
+
+    //get
+    assert.deepEqual(viewModel.getColumns(), [], "getColumns");
+    assert.deepEqual(viewModel.getColumnById(1), undefined, "getColumnById");
+    assert.deepEqual(viewModel.getColumnByIndex(1), undefined, "getColumnByIndex");
+    assert.deepEqual(viewModel.getColumnIndexById(1), -1, "getColumnIndexById");
+
+    assert.deepEqual(viewModel.getRows(), [], "getRows");
+    assert.deepEqual(viewModel.getRowById(1), undefined, "getRowById");
+    assert.deepEqual(viewModel.getRowByIndex(1), undefined, "getRowByIndex");
+    assert.deepEqual(viewModel.getRowIndexById(1), -1, "getRowIndexById");
+
+    assert.deepEqual(viewModel.getColumnsWidth(0), 0, "getColumnsWidth");
+    assert.deepEqual(viewModel.getRowsHeight(0), 0, "getRowsHeight");
+
+
 });
 
 
